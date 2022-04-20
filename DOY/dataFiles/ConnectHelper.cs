dc5bdc81df7e1b512bb555144060d11cd42ad4f0
Илья Шадрин <iliasadrin2002@yandex.ru>
2022-04-20 16:15:28 +0300

    Добавьте файлы проекта.

---
+++ DOY/dataFiles/ConnectHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOY.dataFiles
+{
+    internal class ConnectHelper
+    {
+        public static DOYEntities1 entObj;   
+    }
+}
